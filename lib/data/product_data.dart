92127363525b05a495578dc0f024e81ec07faf4c
IRAM_ERNESTO_MARTINEZ_RUIZ <a.21308051280378@cbtis128.edu.mx>
2024-04-24 15:53:01 +0000

    actualizadoo

---
+++ lib/data/product_data.dart
@@ -0,0 +1,16 @@
+import 'package:uii_act6_disenio/modelos/product_model.dart';
+
+List<ProductModel> products = [
+  ProductModel(
+      img: "assets/images/comedor.png",
+      title: "Comedor redondo",
+      description: "Comedor redondo con 2 sillas"),
+  ProductModel(
+      img: "assets/images/sala.png",
+      title: "Sala comoda",
+      description: "Sala color verde para la familia"),
+  ProductModel(
+      img: "assets/images/sillon.png",
+      title: "Sillon de gamusa",
+      description: "Sillon de gamusa muy comodo"),
+];
